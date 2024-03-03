52b97be6426d5a51ab9f1de45163010669a4fb45
Bekzat Saparbekov <157128160+Bekzat707@users.noreply.github.com>
2024-03-03 11:49:26 +0600

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,4 +1,11 @@
-import re
+import re 
+
+
+with open("row.txt") as f:
+    text = f.read()
+
+print("TASK 1:")
+
 
 def reges(text):
     reg='ab*'
@@ -6,12 +13,11 @@ def reges(text):
             return True
     else:
             return False
-text=["abb","abab","abbb","abbbbb","abs"]
 for i in text:
     print(f"{i}: {reges(i)}")
 
 
-
+print("TASK 2:")
 
 def reges(text):
     reg='ab{2,3}'
@@ -19,19 +25,17 @@ def reges(text):
             return True
     else:
             return False
-text=["abb","abab","abbb","abbbbb","abs"]
 for i in text:
     print(f"{i}: {reges(i)}")
 
 
-
+print("TASK 3:")
 
 def find_sequences(text):
       pattern=r'\b[a-z]+_[a-z]+\b'
       sequences=re.findall(pattern,text)
       return sequences
 def main():
-      text=input("INPUT TEXT: ")
       seq=find_sequences(text)
       
       if seq:
@@ -45,14 +49,13 @@ if __name__=="__main__":
       main()      
 
 
-
+print("TASK 4:")
 
 def find_up_let(text):
       pattern=r'\b[A-Z][a-z]+\b'
       up_let=re.findall(pattern,text)
       return up_let
 def main():
-      text=input("INPUT TEXT: ")
       up_let=find_up_let(text)
       if up_let:
             for i in up_let:
@@ -64,14 +67,13 @@ if __name__=="__main__":
       main()   
 
 
-
+print("TASK 5:")
 
 def ending(text):
       pettern=r'a[a-z]*b'
       ending=re.findall(pettern,text)
       return ending
 def main():
-      text=input("INPUT TEXT: ")
       end=ending(text)
       if end:
             for i in end:
@@ -83,7 +85,7 @@ if __name__=="__main__":
       main()
 
 
-
+print("TASK 6:")
 
 def replace(text):
       replace_word={" ":":",",":":",".":":"}
@@ -91,35 +93,33 @@ def replace(text):
             text=text.replace(old_word,new_word)
       return text
 if __name__=="__main__":
-      text=input("ENTER TEXT: ")
       new_text=replace(text)
       print("NEW TEXT:",new_text)
 
 
-
+print("TASK 7:")
 
 def snake(snake_case):
       words=snake_case.split('_')
       camel_case=words[0]+''.join(word.capitalize() for word in words[1:])
       return camel_case
 if __name__=="__main__":
-      words=input("Enter snake case string: ")
-      camel_case=snake(words)
+
+      camel_case=snake(text)
       print("Camel case string:",camel_case)
 
 
-
+print("TASK 8:")
 
 def split_up(text):
       split=re.findall('[A-Z][a-z]*',text)
       for i in split:
             print(i)
 if __name__=="__main__":
-      text=input()
       split_up(text)
 
 
-
+print("TASK 9:")
 
 def split_up(text):
       split=re.findall('[A-Z][a-z]*',text)
@@ -127,11 +127,11 @@ def split_up(text):
             print(i,end=" ")
       print()
 if __name__=="__main__":
-      text=input()
       split_up(text)
 
 
 
+print("TASK 10:")
 
 def camel_to_snake(camel_case):
     snake_case = ""
@@ -143,6 +143,7 @@ def camel_to_snake(camel_case):
     return snake_case.lstrip('_')
 
 if __name__ == "__main__":
-    camel_case_input = input("Enter a camel case string: ")
-    snake_case_output = camel_to_snake(camel_case_input)
+    snake_case_output = camel_to_snake(text)
     print("Snake case string:", snake_case_output)
+
+f.close()
